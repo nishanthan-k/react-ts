fb1e8bf459511435cab828a49640e081fc7dd2c8
nishanthan-k <krn.nishanthan@gmail.com>
2024-08-17 11:10:57 +0530

    feat: eslint, tailwind config

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-2xl text-red-500'>Hello</h1>
+      <form>
+        <input type='text'/>
+      </form>
+    </>
+  );
+}
+
+export default App;
